52b9b65c2b10da4af81d2f82b5be05e76f378048
Wenda Zhang <vaaandark@gmail.com>
2025-01-25 00:00:22 +0800

    Change to shut down upon receiving a SIGTERM signal, but with a three-second delay for graceful shutdown

--- nix/nix.go
+++ nix/nix.go
@@ -1,20 +1,25 @@
 package nix
 
 import (
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func SetupSignalHandler() (stopCh <-chan struct{}) {
 	stop := make(chan struct{})
-	c := make(chan os.Signal, 2)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-c
 		close(stop)
-		<-c
-		os.Exit(1) // second signal. Exit directly.
+		for i := 3; i > 0; i-- {
+			log.Printf("There are %ds left to exit", i)
+			time.Sleep(1 * time.Second)
+		}
+		os.Exit(1)
 	}()
 	return stop
 }
