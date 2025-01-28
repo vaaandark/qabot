acd494880c2d13b90681257d13703883c7c567c9
Wenda Zhang <vaaandark@gmail.com>
2025-01-28 15:20:05 +0800

    No delay to stop

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,12 +1,10 @@
 package util
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"strconv"
 	"syscall"
-	"time"
 )
 
 func SetupSignalHandler() (stopCh <-chan struct{}) {
@@ -16,10 +14,10 @@ func SetupSignalHandler() (stopCh <-chan struct{}) {
 	go func() {
 		<-c
 		close(stop)
-		for i := 3; i > 0; i-- {
-			log.Printf("There are %ds left to exit", i)
-			time.Sleep(1 * time.Second)
-		}
+		// for i := 3; i > 0; i-- {
+		// 	log.Printf("There are %ds left to exit", i)
+		// 	time.Sleep(1 * time.Second)
+		// }
 		os.Exit(1)
 	}()
 	return stop
